f1024aea2fac0bbff518ce5563385cfa54fec512
Hudson Reich <river@hudsons-air.mynetworksettings.com>
2023-10-09 11:35:06 -0400

    Test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 from tqdm import tqdm
-import os
-import requests
 import time
+import requests
+
 quote_list = []
 idex = 0
 lve = requests.get("https://raw.githubusercontent.com/TogiFerretFerret/test-e/main/jokes/vimjokes.txt").text.split("\n")
@@ -12,8 +12,14 @@ for i in tqdm(range(len(lve)),desc="Downloading Quotes"):
     idex+=1
 print(Fore.GREEN + "Done!")
 print(Style.RESET_ALL)
+# Create a function to print a string with a typewriter effect
+def typewriter(string,speed=0.1):
+    for char in string:
+        print(Fore.CYAN + char, end='')
+        time.sleep(speed)
+    print()
 quote_list.pop(len(quote_list)-1)
 for i in quote_list:
-    print(Fore.CYAN + i)
+    typewriter(i)
     print(Style.RESET_ALL)
-
+    input("Press Enter to continue...")
